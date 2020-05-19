ef647b0097682cae1abb352e69a208578a06f34f
Dan Sheffner <Dan@Sheffner.com>
2020-05-19 17:07:12 -0500

    changing these around so they will build correctly

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "build.sh started..."
-docker rmi thesheff17/vim_testing:latest
-time docker build -f Dockerfile_testing -t thesheff17/vim_testing:latest .
+docker rmi thesheff17/vim-testing:latest
+time docker build -f Dockerfile-testing -t thesheff17/vim-testing:latest .
 echo "build.sh completed"
